78cede89f9d7b60d6d93a4ae528223ca24426429
Aleksei Mihalin <warmwavemedia@gmail.com>
2022-04-01 17:46:20 +0300

    update engine

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,17 +9,17 @@ SITESUBTITLE = 'Компы и сети, тела и души, шумы и зву
 SITEURL = ''
 DEFAULT_LANG = 'ru'
 
+FAVICON = '/theme/images/favicon.png'
+THEME = 'themes/aboutwilson_my/'
+
 PATH = 'content'
 OUTPUT_PATH = 'output'
 DELETE_OUTPUT_DIRECTORY = True
 DEFAULT_PAGINATION = 10
 RELATED_POSTS_MAX = 5
 
-
 TIMEZONE = 'Europe/Moscow'
 CURRENTYEAR = datetime.now().strftime('%Y')
-
-
 
 PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
@@ -31,13 +31,13 @@ TAG_URL = 'tag/{slug}'
 TAG_SAVE_AS = 'tag/{slug}/index.html'
 AUTHOR_SAVE_AS = ''
 
-STATIC_PATHS = ['files', 'style.css', 'uploads']
+STATIC_PATHS = ['files', 'style.css', 'robots.txt']
 
 EXTRA_PATH_METADATA = {
     # 'themes/aboutwilson_my/static/': {'path': 'theme'},
     'style.css': {'path': 'style.css'},
     'files': {'path': 'files'},
-    'uploads': {'path': 'uploads'},
+    'robots.txt': {'path': 'robots.txt'}
     # 'extra/CNAME': {'path': 'CNAME'},
 
 }
@@ -49,24 +49,17 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# # Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+#
+# # Social widget
+# SOCIAL = (('twitter', 'https://twitter.com/paulomiramor'),
+#           ('linkedin', 'http://www.linkedin.com/in/XXXXXXX'),
+#           ('github', 'https://github.com/EuPaulo'),)
 
-# Social widget
-SOCIAL = (('twitter', 'https://twitter.com/paulomiramor'),
-          ('linkedin', 'http://www.linkedin.com/in/XXXXXXX'),
-          ('github', 'https://github.com/EuPaulo'),)
-
-
-
-FAVICON = '/theme/images/favicon.png'
-
-THEME = 'themes/aboutwilson_my/'
-# THEME = 'themes/mnmlist/'
-# THEME = 'themes/nmnlist/'
 
 DISPLAY_PAGES_ON_MENU = True
 DISPLAY_CATEGORIES_ON_MENU = True
@@ -85,6 +78,9 @@ MENUITEMS = (
 
 MENUITEMS2 = (
     ('ВСЁ', '/archives.html'),
+)
+
+MENUITEMS3 = (
     ('СКАЗАТЬ "СПАСИБО!"', '/donate.html'),
 )
 
@@ -99,7 +95,8 @@ MENUITEMS2 = (
 PLUGIN_PATH = "plugins"
 # PLUGINS = ['sitemap', 'neighbors', 'summary', 'related_posts', 'optimize_images', 'disqus']
 # PLUGINS = ['sitemap', 'category_order', 'w3c_validate', 'optimize_images', '']
-PLUGINS = ['neighbors', 'summary', 'related_posts', 'optimize_images', 'disqus_static', 'sitemap']
+# PLUGINS = ['neighbors', 'summary', 'related_posts', 'optimize_images', 'disqus_static', 'sitemap']
+PLUGINS = ['neighbors', 'summary', 'related_posts', 'optimize_images', 'sitemap']
 # disqus_static
 # sitemap
 # gzip_cache
@@ -137,5 +134,3 @@ DISQUS_PUBLIC_KEY = 'S6SsIA4Uq86sqe3sIvcjvOqcGi3lM9sHPkz2tiK3Wy3mick3zIcw3T63OXj
 
 # GOOGLE_ANALYTICS = 'UA-******your_code'
 # GOOGLE_ANALYTICS_DOMAIN = 'http://webquant.ru/'
-
-
